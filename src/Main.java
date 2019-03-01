3a908dddc6f819105278da06fa3c6ad4faeb2ad9
MikhailoMokryy <m.mokryy@ukma.edu.ua>
2019-03-01 18:04:44 +0200

    resolve conflict

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-        Runnable hello_world = () ->  System.out.printf("Hello %s!","world");
+        Runnable hello_world = () ->  System.out.printf("Hello %s!","world by Nazar Kostiuk");
         hello_world.run();
 
-=======
-        System.out.printf("Hello %s!","world by Nazar Kostiuk");;
-        
->>>>>>> 6d8370493dd1c1b01ff20ec2614bd281c0dc0f1b
     }
 
 

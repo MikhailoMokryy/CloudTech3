6d8370493dd1c1b01ff20ec2614bd281c0dc0f1b
Nazar Kostiuk <n.kostiuk@ukma.edu.ua>
2019-03-01 14:45:15 +0200

    Add error by Nazar Kostiuk

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.printf("Hello %s!","world");
+        System.out.printf("Hello %s!","world by Nazar Kostiuk");;
         
     }
 
